678580d7c82a60e0fdc8310e8652c2290cfc36d8
devopsabi <devopsabi@gmail.com>
2019-12-01 04:30:53 +0000

    updated color to red

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,7 +5,7 @@ const port = 3000
 var os = require("os");
 var hostname = os.hostname();
 
-app.get('/', (req, res) => res.send('Hello World! <h1 style="color:yellow;">Demo App Version 1<h1> <br>'))
+app.get('/', (req, res) => res.send('Hello World! <h1 style="color:red;">Demo App Version 1<h1> <br>'))
 
 app.get('/api',(req,res)=>{
     res.json({title:"api",message:"root", host:hostname});
