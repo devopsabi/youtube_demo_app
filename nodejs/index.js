818468ffd1d26dff0fa85b2c5f7e2edff290ad11
devopsabi <devopsabi@gmail.com>
2019-12-01 01:41:49 +0000

    updated color green

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,5 +5,9 @@ const port = 3000
 var os = require("os");
 var hostname = os.hostname();
 
-app.get('/', (req, res) => res.send('Hello World! <h1 style="color:red;">Demo App Version 1<h1> <br>'))
+app.get('/', (req, res) => res.send('Hello World! <h1 style="color:green;">Demo App Version 1<h1> <br>'))
+
+app.get('/api',(req,res)=>{
+    res.json({title:"api",message:"root", host:hostname});
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
